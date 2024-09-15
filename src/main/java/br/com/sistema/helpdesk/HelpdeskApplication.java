11769b70082f3515eff8d85d6c9095ec8791a956
Lai-cunha <lai_cunha@outlook.com>
2024-09-14 23:48:15 -0300

    master: carga inicial da base de dados

--- src/main/java/br/com/sistema/helpdesk/HelpdeskApplication.java
+++ src/main/java/br/com/sistema/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,48 @@
 package br.com.sistema.helpdesk;
 
+import br.com.sistema.helpdesk.domain.damain.Chamado;
+import br.com.sistema.helpdesk.domain.damain.Cliente;
+import br.com.sistema.helpdesk.domain.damain.Tecnico;
+import br.com.sistema.helpdesk.domain.enums.Perfil;
+import br.com.sistema.helpdesk.domain.enums.Prioridade;
+import br.com.sistema.helpdesk.domain.enums.Status;
+import br.com.sistema.helpdesk.repositories.ChamadoRepository;
+import br.com.sistema.helpdesk.repositories.ClienteRepository;
+import br.com.sistema.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Tecnico tec1 = new Tecnico(null, "Rogerio Silva", "111.111.111-11", "rogerio@mail.com", "123");
+		tec1.addPerfis(Perfil.ADMIN);
+
+		Cliente cli1 = new Cliente(null, "Mariana Silva", "175.111.111-12", "mariana@mail.com", "321");
+		cli1.addPerfis(Perfil.CLIENTE);
+
+        Chamado chamado1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 1", "Primeiro chamado", tec1, cli1);
+    	tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(chamado1));
+	}
 }
